9bd52996c4d094808082b8d2d14e92f4117a90d1
Ken Crocken <kcrocken@gmail.com>
2014-05-01 00:01:37 -0400

    Fixed Rspec, started Welcome test

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe "Welcome" do
+
+  let(:base_title) { "KagyuDC" }
+
+  describe "welcome page" do
+
+    it "should have the content 'Kagyu Drupgyu Chodzong'" do
+      visit root_path
+      expect(page).to have_content("Kagyu Drupgyu Chodzong")
+    end
+
+    it "should have the title 'Welcome'" do
+      visit root_path
+      expect(page).to have_title(full_title('Welcome'))
+    end
+
+    it "should have the right links on the layout" do
+      visit root_path
+        click_link "About"
+        expect(page).to have_title(full_title('About'))
+        click_link "Contact"
+        expect(page).to have_title(full_title('Contact'))
+        click_link "Home"
+        expect(page).to have_title(full_title('Welcome'))
+        click_link "KagyuDC"
+        expect(page).to have_title(full_title('Welcome'))
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,13 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
-require 'minitest/autorun'
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   # ## Mock Framework
@@ -21,6 +17,7 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
+
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -40,6 +37,4 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
-  config.include Capybara::DSL
-
 end

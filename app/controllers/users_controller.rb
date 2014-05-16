358d518cd8abf67088d951853b34425bfd254aab
Ken Crocken <kcrocken@gmail.com>
2014-05-16 18:28:37 -0400

    added user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update, :show]
-  before_action :correct_user,   only: [:edit, :update, :show]
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def index
+    @users = User.all
   end
 
   def show
@@ -46,8 +47,9 @@ class UsersController < ApplicationController
 
     def signed_in_user
       unless signed_in?
+        flash[:warning] = "Please sign in."
         store_location
-        redirect_to signin_path, warning: "Please sign in."
+        redirect_to signin_path
       end
     end
 

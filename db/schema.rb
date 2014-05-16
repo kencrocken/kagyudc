853c66e2a9ca6a5e4193c6b74f7075def1ce8e24
Ken Crocken <kcrocken@gmail.com>
2014-05-16 12:36:18 -0400

    Admin added to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140502201445) do
+ActiveRecord::Schema.define(version: 20140516162746) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140502201445) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
